fd2d9791d746845d00bfa8187048557c6317bda1
Oleg Storozhev <54131483+Oleg-Storozhev@users.noreply.github.com>
2021-04-10 14:51:14 +0300

    Delete dbConfig.java

--- src/main/java/org/hillel/homework_1/dbConfig.java
+++
@@ -1,9 +0,0 @@
-package org.hillel.homework_1;
-
-public class dbConfig {
-    protected final String dbHost = "localhost";
-    protected final String dbPort = "3306";
-    protected final String dbUser = "root";
-    protected final String dbPass = "";
-    protected final String dbName = "dbtickets";
-}
